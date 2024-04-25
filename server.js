af6442fc484b252b341538522fa673a6b32538b2
Edmond-Amofah-Boakye <80597593+Edmond-Amofah-Boakye@users.noreply.github.com>
2024-04-25 09:46:29 +0000

    base commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express()
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+    cors: "*"
+})
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("message", (data)=>{
+        console.log(`this is totally from ${data}`);
+    })
+})
+
+httpServer.listen(3000, () => console.log("server listening on port 3000"))
